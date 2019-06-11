7ea186105308e8e4cc990661c318ec36011f822d
Charlie Bickerton <charlie@mimin.al>
2019-06-11 14:31:36 +0100

    ran npm run build and react static files are in the correct place and display when running flask

---
+++ flask/static/react/precache-manifest.4fce802b83485f9364aaec841ff1e9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd1c3359dd93ea496a0022478cd7944",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "ba2dde39de79dd576e43",
+    "url": "/static/react/css/main.3caf16e5.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/react/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "ba2dde39de79dd576e43",
+    "url": "/static/react/js/main.8c856ec7.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
